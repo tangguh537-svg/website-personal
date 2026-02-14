015f3833835fb8263d69525fc1c4d227ffe3d43a
tangguh537-svg <tangguh537@gmail.com>
2026-02-14 14:37:26 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,30 @@
+const nama ="tangguh satura";
+let usia = 28;
+
+let biodata = document.getElementById('biodata')
+console.log(biodata);
+
+function generateBiodata() {
+    let generasi;
+
+    if (usia > 10 && usia < 18) {
+       generasi ="generasi remaja"
+    }
+    else if (usia > 18 && usia < 30) {
+       generasi ="generasi dewasa"
+    }
+    else if (usia > 2 && usia < 10) {
+       generasi ="generasi bocah"
+    }
+    
+    else {
+       generasi ="generasi baayi"
+    }
+    return biodata.innerHTML = generasi;
+
+}
+   
+console.log(nama);
+console.log(usia);
+
+generateBiodata();
